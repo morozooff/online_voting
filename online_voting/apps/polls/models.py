cf5f8e571b3c3e830381f3041d8835bf0e6c09d5
Morozov Yaroslav <yar1m@mail.ru>
2024-10-06 19:30:09 +0300

    feat: add main models of project

--- online_voting/apps/polls/models.py
+++ online_voting/apps/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=datetime.now)
+
+class  Choice(models.Model):
+    text = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+    poll_id = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    
